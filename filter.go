f37682a6e3e1c9af7e87bd63082a62c581fd2ea4
MrBlackk <volodymyr.chornyj@gmail.com>
2020-11-30 11:33:48 +0200

    optimized filtering and converting words to simple form

--- filter.go
+++ filter.go
@@ -6,38 +6,23 @@ import (
 	"unicode"
 )
 
-// TODO: optimize to one-two loops and check order
 func prepare(str string) []string {
 	arr := strings.FieldsFunc(str, func(r rune) bool {
 		return !unicode.IsLetter(r)
 	})
-	arr = lowercase(arr)
-	arr = filter(arr)
-	arr = stem(arr)
-	arr = replaceSynonyms(arr)
-	return arr
+	return filter(arr)
 }
 
-func lowercase(words []string) []string {
-	r := make([]string, len(words))
-	for i, word := range words {
-		r[i] = strings.ToLower(word)
-	}
-	return r
-}
-
-func stem(words []string) []string {
-	r := make([]string, len(words))
-	for i, word := range words {
-		r[i] = stemmer.Stem(word, false)
-	}
-	return r
-}
-
-func replaceSynonyms(words []string) []string {
-	r := make([]string, len(words))
-	for i, word := range words {
-		r[i] = getBaseSynonymOrSelf(word)
+func filter(words []string) []string {
+	r := words[:0]
+	for i := range words {
+		word := strings.ToLower(words[i])
+		word = stemmer.Stem(word, false)
+		if isInStopLists(word) {
+			continue
+		}
+		word = getBaseSynonymOrSelf(word)
+		r = append(r, word)
 	}
 	return r
 }
@@ -49,16 +34,6 @@ func getBaseSynonymOrSelf(word string) string {
 		}
 	}
 	return word
-}
-
-func filter(words []string) []string {
-	r := make([]string, 0, len(words))
-	for _, word := range words {
-		if !isInStopLists(word) {
-			r = append(r, word)
-		}
-	}
-	return r
 }
 
 func isInStopLists(word string) bool {
